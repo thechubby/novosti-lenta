763ed5d02d0b36025e1272fc5963fa7855986b12
Christine <31845754+thechubby@users.noreply.github.com>
2018-09-03 16:41:59 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var news = [
+  {
+    author: 'Кристина',
+    text: 'Ля-ля-ля, мне нечего сказать'
+  }, {
+    author: 'Иван',
+    text: 'Жить стало проще, жить стало веселее'
+  }, {
+    author: 'Александр',
+    text: 'Абырвалг'
+  }
+];
+
+function App() {
+  return (
+    <div>
+      тест
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
